8bccf8841b8f25215d06ca270d33d3aa7f814287
Warren Bartholomew <wjbartholomew@hotmail.com>
2020-05-17 16:58:26 -0500

    base functionality complete

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ $(document).ready(readyNow); // scripts to run on page load
 
 function readyNow(){  // adding functionality to button click
     $('#submitButton').on('click', submitData);
+    $('#employees').on('click', '#deleteButton', deleteEmployee);
 }
 
 let employeeData = [];  // Global array to hold employee data from DOM inputs
@@ -13,7 +14,7 @@ function submitData(){ // function to get user input data
     let title = $('#title').val();
     let annualSalary = $('#annualSalary').val();
     employeeData.push({firstName,lastName,id,title,annualSalary})
-    $('#employees').append(`<tr><th>${firstName}</th><th>${lastName}</th><th>${id}</th><th>${title}</th><th>${annualSalary}</th></tr>`)
+    $('#employees').append(`<tr><td>${firstName}</td><td>${lastName}</td><td>${id}</td><td>${title}</td><td>${annualSalary}</td><td><button id="deleteButton">Delete</button></td></tr>`)
     $('#firstName').val('');
     $('#lastName').val('');
     $('#id').val('');
@@ -31,7 +32,15 @@ function totalMonthlyCosts(){
         console.log(monthlyCosts);
     }
     if (monthlyCosts > 20000){
-        $('#monthlyCosts').addClass(); 
+        $('#monthlyCosts').removeClass("monthlyCostsLow").addClass("monthlyCostsHigh"); 
+    }
+    else if (monthlyCosts <= 20000){
+        $('#monthlyCosts').addClass("monthlyCostsLow"); 
     }
     $('#monthlyCosts').append(monthlyCosts);
+}
+
+function deleteEmployee(){
+    console.log('in delete')
+    $(this).parent().parent().remove();
 }
